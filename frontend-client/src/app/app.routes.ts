f7564eeec329343de65754deb26c3345696d737f
Ndlovu-luyanda <ndlovuluyanda606@gmail.com>
2026-08-21 23:15:29 +0200

    Removed Landing-Page from shared

--- frontend-client/src/app/app.routes.ts
+++ frontend-client/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LandingPage } from './shared/components/landing-page/landing-page';
+import { LandingPage } from './features/landing-page/landing-page';
 import { Footer } from './layout/footer/footer';
 import { Header } from './layout/header/header';
 
--- frontend-client/src/app/features/landing-page/landing-page.ts
+++ frontend-client/src/app/features/landing-page/landing-page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Header } from '../../../layout/header/header';
-import { Footer } from '../../../layout/footer/footer';
+import { Header } from '../../layout/header/header';
+import { Footer } from '../../layout/footer/footer';
 
   @Component({
     selector: 'app-landing-page',
