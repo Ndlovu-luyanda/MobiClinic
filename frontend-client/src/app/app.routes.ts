2534b9a8f0578ecbb545e084c9d0a5cd438b9c35
Ndlovu-luyanda <ndlovuluyanda606@gmail.com>
2026-08-21 22:40:47 +0200

    Footer and Header
    Header and footer implementation

--- frontend-client/src/app/app.routes.ts
+++ frontend-client/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './shared/components/landing-page/landing-page';
+import { Footer } from './layout/footer/footer';
+import { Header } from './layout/header/header';
 
 export const routes: Routes = [
-    {path : '', component : LandingPage}
+    {path : '', component : LandingPage},
+    {path : '',component: Footer},
+    {path : '', component : Header}
 ];
--- frontend-client/src/app/shared/components/landing-page/landing-page.ts
+++ frontend-client/src/app/shared/components/landing-page/landing-page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Header } from '../../../layout/header/header';
+import { Footer } from '../../../layout/footer/footer';
 
-@Component({
-  selector: 'app-landing-page',
-  imports: [],
-  templateUrl: './landing-page.html',
-  styleUrl: './landing-page.scss',
-})
-export class LandingPage {
+  @Component({
+    selector: 'app-landing-page',
+    standalone: true,
+    imports: [Header, Footer],
+    templateUrl: './landing-page.html',
+    styleUrl: './landing-page.scss',
+  })
+  export class LandingPage {
 
 }
